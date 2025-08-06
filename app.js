b2e103c29e1af03bc82f915c0723cb0384c8ff5c
Carlos Alberto Junior <carlosjn400@gmail.com>
2025-08-06 14:27:02 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 
+document.querySelector('.container__input').addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    verificarChute();
+  }})
+
+function nomeInicial(){
+ let nome = prompt('Qual o seu nome? ')
+ return nome;
+}
+
+
 
 function exibirTextoNaTela(tag,texto){
   let campo = document.querySelector(tag);
@@ -13,7 +24,8 @@ function exibirTextoNaTela(tag,texto){
 
 function exibirMensagemInicial(){
   exibirTextoNaTela('h1','Jogo do Numero Secreto')
-exibirTextoNaTela('p', `Escolha um numero entre 1 e ${numeroLimite}`)
+  exibirTextoNaTela('h2',`Olá ${nomeInicial()} seja bem vindo!`)
+exibirTextoNaTela('p', `Escolha um numero entre 1 e ${numeroLimite} e veja se você consegue acertar o numero que estou pensando!`)
 }
 
 exibirMensagemInicial();
@@ -24,16 +36,16 @@ exibirMensagemInicial();
 function verificarChute(){
   let chute =  document.querySelector('input').value
   if(chute == numeroSecreto){
-    exibirTextoNaTela('h1','Acertou!')
+    exibirTextoNaTela('h1','Parabéns, você Acertou!')
     let palavraTentativa = tentativas > 1 ? 'tentativas':'tentativa';
     let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
     exibirTextoNaTela('p',mensagemTentativas)
     document.getElementById('reiniciar').removeAttribute('disabled')
   }else{
     if(chute > numeroSecreto){
-      exibirTextoNaTela('p','O numero secreto é menor')
+      exibirTextoNaTela('p','IIh ERROU! O numero secreto é menor')
     }else{
-      exibirTextoNaTela('p','O numero secreto é maior')
+      exibirTextoNaTela('p','IIh ERROU!O numero secreto é maior')
     }
     tentativas++
     limparCampo();
@@ -68,5 +80,28 @@ function reiniciarJogo(){
   tentativas = 1;
   exibirMensagemInicial()
   document.getElementById('reiniciar').setAttribute('disabled',true)
+
   
+}
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+document.onkeydown = function(e) {
+  // Desabilita F12
+  if (e.key === "F12" || e.keyCode === 123) {
+    e.preventDefault();
+  }
+  // Desabilita Ctrl+Shift+I
+  if (e.ctrlKey && e.shiftKey && (e.key === "I" || e.keyCode === 73)) {
+    e.preventDefault();
+  }
+  // Desabilita Ctrl+Shift+J
+  if (e.ctrlKey && e.shiftKey && (e.key === "J" || e.keyCode === 74)) {
+    e.preventDefault();
+  }
+  // Desabilita Ctrl+U
+  if (e.ctrlKey && (e.key === "U" || e.keyCode === 85)) {
+    e.preventDefault();
+  }
 }
